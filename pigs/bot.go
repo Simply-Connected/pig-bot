e9a73afbc3a4874742aad82cc1e180fbef1d3bb5
Alexandr Eremin <eremin.casha@gmail.com>
2022-07-26 13:09:12 +0400

    fix rename and returns

--- pigs/bot.go
+++ pigs/bot.go
@@ -44,8 +44,7 @@ func NewBot(params *Params) (*tele.Bot, error) {
 		if user.Pig.LastGrow.Valid &&
 			curTime.Day() == user.Pig.LastGrow.Time.Day() &&
 			curTime.Sub(user.Pig.LastGrow.Time).Hours() < 24 {
-			c.Send(fmt.Sprintf("Вы уже кормили свою свинью сегодня.\n\nВес вашего свина: *%d*", user.Pig.Weight), tele.ModeMarkdown)
-			return nil
+			return c.Send(fmt.Sprintf("Вы уже кормили свою свинью сегодня.\n\nВес вашего свина: *%d*", user.Pig.Weight), tele.ModeMarkdown)
 		}
 
 		diff := random.Int31n(16) - 5
@@ -53,26 +52,26 @@ func NewBot(params *Params) (*tele.Bot, error) {
 			user.Pig.Weight = 7
 			user.Pig.LastGrow = sql.NullTime{Valid: false}
 			db.Save(&user.Pig)
-			c.Send(fmt.Sprintf("Ваша свинья *%s* потеряла весь вес и умерла ☠️. "+
+			return c.Send(fmt.Sprintf("Ваша свинья *%s* потеряла весь вес и умерла ☠️. "+
 				"Вы получаете нового порося.\n\n"+
 				"Вес вашего свина: *%d*",
 				user.Pig.Name, user.Pig.Weight), tele.ModeMarkdown)
-			return nil
 		}
 		user.Pig.Weight = uint32(int32(user.Pig.Weight) + diff)
 		user.Pig.LastGrow = sql.NullTime{Time: time.Now(), Valid: true}
 		db.Save(&user.Pig)
-		c.Send(getGrowPhrase(&user.Pig, diff), tele.ModeMarkdown)
-		return nil
+		return c.Send(getGrowPhrase(&user.Pig, diff), tele.ModeMarkdown)
 	})
 
 	b.Handle("/rename", func(c tele.Context) error {
+		if c.Message().Payload == "" {
+			return c.Send("Вы ввели пустое имя. У свинки должно быть полноценное имя")
+		}
 		user := GetOrRegisterUser(c, db)
 
 		user.Pig.Name = c.Message().Payload
 		db.Save(&user.Pig)
-		c.Send(fmt.Sprintf("Теперь вашу свинью зовут *%s*", user.Pig.Name), tele.ModeMarkdown)
-		return nil
+		return c.Send(fmt.Sprintf("Теперь вашу свинью зовут *%s*", user.Pig.Name), tele.ModeMarkdown)
 	})
 
 	b.Handle("/top", func(c tele.Context) error {
@@ -85,8 +84,7 @@ func NewBot(params *Params) (*tele.Bot, error) {
 		for i, pig := range pigs {
 			message.WriteString(fmt.Sprintf("*%d*. *%s* - %d кг\n", i+1, pig.Name, pig.Weight))
 		}
-		c.Send(message.String(), tele.ModeMarkdown)
-		return nil
+		return c.Send(message.String(), tele.ModeMarkdown)
 	})
 	return b, nil
 }
